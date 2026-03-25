ca9b421ebc25b4aa6307b8f5e60b437ff01e6e8d
Carlos Hilario <hilario.carlos.prac@gmail.com>
2026-03-25 11:26:19 -0500

    subida src

---
+++ Public/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Registro from "./components/Registro";
+import Inicio from "./components/Inicio";
+import Documentacion from "./components/Documentacion-DNI";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="content">
+          <Routes> 
+            <Route path="/registro" element={<Registro />} /> 
+            <Route path="/" element={<Inicio />} />
+            <Route path="/documentacion" element={<Documentacion />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+export default App;
